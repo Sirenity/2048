2e3e8f900b4432066c477f04de084f3cb6c32bf3
Stephen Rodriguez <steprodriguez10@gmail.com>
2014-04-09 21:54:22 -0400

    checkpoint from here

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
-animationDelay = 100;
-minSearchTime = 20;
+animationDelay = 200;
+minSearchTime = 40;
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
